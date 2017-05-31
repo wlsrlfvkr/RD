887e6a1f135aff3097cae3420145e310c47ca1b1
kim byung <kog5071@naver.com>
2017-05-31 11:21:35 +0900

    Basic Setting (Place Node & Grounds , Make WayPoints & Start,EndingPoint , Make Enemy & Enable Simple AI)

---
+++ Assets/2.Scripts/CsEnemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CsEnemy : MonoBehaviour {
+
+	public float speed = 10f;
+
+	private Transform target;
+	private int wavePointIndex;
+
+	void Start () { 
+		
+		target = CsWayPoint.points [0];
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Vector3 dir = target.position - transform.position;
+		transform.Translate (dir.normalized * speed * Time.deltaTime,Space.World);
+
+		if (Vector3.Distance(transform.position, target.position) <= 0.2f) {
+			GetNextWaypoint ();
+		}
+
+	}
+
+	void GetNextWaypoint() {
+		if (wavePointIndex >= CsWayPoint.points.Length - 1) {
+			Destroy (gameObject);
+			return;
+		}
+		wavePointIndex++;
+		target = CsWayPoint.points [wavePointIndex];
+	}
+}
